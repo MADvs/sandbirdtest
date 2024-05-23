0ca04be4310c25ae5886098e90ce19c3d73b689a
Michael <michaelaledav@gmail.com>
2024-05-23 04:18:09 -0400

    Version 0.3.1

--- script.js
+++ script.js
@@ -4,25 +4,43 @@ const menuIcon = document.getElementById('menuIcon');
 const svgFill = document.querySelectorAll('.svg-fill');
 const darkToggle = document.getElementById('darkToggle');
 const darkLabel = document.getElementById('darkLabel');
+let darkMode = localStorage.getItem("darkMode");
 
+// Enable Dark Mode
+const enableDarkMode = () => {
+    logo.setAttribute('src', "/images/logos/darklogo.svg");
+    menuIcon.setAttribute('src', '/images/icons/menu-icon-dark.svg');
+    darkLabel.innerHTML = "DARK";
+    for (const obj of svgFill) {
+        obj.setAttribute('fill', '#F99E3C');
+    }
+    body.classList.add('dark');
+    window.localStorage.setItem("darkMode", "enabled")
+}
+
+// Disable Dark Mode
+const disableDarkMode = () => {
+    logo.setAttribute('src', "/images/logos/lightlogo.svg");
+    menuIcon.setAttribute('src', '/images/icons/menu-icon-light.svg');
+    darkLabel.innerHTML = "LIGHT";
+    for (const obj of svgFill) {
+        obj.setAttribute('fill', '#A1061D');
+    }
+    body.classList.remove('dark');
+    window.localStorage.setItem("darkMode", null)
+}
+
+if (darkMode == "enabled") {
+    enableDarkMode();
+}
+
+// Desktop Button
 function ToggleDarkMode() {
-    if (!body.classList.contains('dark')) {
-        logo.setAttribute('src', "/images/logos/darklogo.svg");
-        menuIcon.setAttribute('src', '/images/icons/menu-icon-dark.svg');
-        darkLabel.innerHTML = "DARK";
-        for (const obj of svgFill) {
-            obj.setAttribute('fill', '#F99E3C');
-        }
-
-        body.classList.add('dark');
+    darkMode = window.localStorage.getItem("darkMode");
+    if (darkMode !== "enabled") {
+        enableDarkMode();
     } else {
-        logo.setAttribute('src', "/images/logos/lightlogo.svg");
-        menuIcon.setAttribute('src', '/images/icons/menu-icon-light.svg');
-        darkLabel.innerHTML = "LIGHT";
-        for (const obj of svgFill) {
-            obj.setAttribute('fill', '#A1061D');
-        }
-        body.classList.remove('dark');
+        disableDarkMode();
     }
 }
 
